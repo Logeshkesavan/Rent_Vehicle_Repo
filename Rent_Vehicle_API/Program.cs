7d567924017b0d7352a084671845c1fa3d2e6be1
Logeshkesavan <162429324+Logeshkesavan@users.noreply.github.com>
2026-04-09 07:55:00 +0530

    commit

--- Rent_Vehicle_API/Program.cs
+++ Rent_Vehicle_API/Program.cs
@@ -15,7 +15,11 @@ builder.Services.AddSwaggerGen();
 
 // Add DbContext
 builder.Services.AddDbContext<VehicleRentalDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null)));
 
 builder.Services
     .AddMcpServer()
